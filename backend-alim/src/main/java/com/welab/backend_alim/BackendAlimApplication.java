ebeddf5de17eebfe25f613c1656196712df57d8e
solarhc <solarhc@naver.com>
2025-05-27 15:58:51 +0900

    최초 작성

---
+++ backend-alim/src/main/java/com/welab/backend_alim/BackendAlimApplication.java
@@ -0,0 +1,13 @@
+package com.welab.backend_alim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAlimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAlimApplication.class, args);
+	}
+
+}
---
+++ backend-alim/src/test/java/com/welab/backend_alim/BackendAlimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welab.backend_alim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAlimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
